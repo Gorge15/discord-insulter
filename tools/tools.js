e9019e03a2ae6dcfa24a9ff0f342be30e67c0675
TotoShefulThau <37251093+TotoShefulThau@users.noreply.github.com>
2021-04-22 13:21:00 +0300

    Update tools.js

--- tools/tools.js
+++ tools/tools.js
@@ -21,11 +21,22 @@ module.exports = {
             "<author> a facut RKO cu pizda ma-tii, <target>",
             "'Face-mi-as schiuri din coastele ma-tii <target>' ~cu drag de la <author>"
         ],
-        "BISERICA": ["<author> a zis ca esti prost/proasta, <target>"],
-        "MORTI": ["<target>, <author> ti-a futut toti mortii si ranitii... Cam necrofil... zic si eu.."],
+        "BISERICA": [
+            "<author> a zis: <target>, sculpt-mi-as pula pe crucea ma-tii",
+            "<author> a exorcizat pizda ma-tii, <target> cu o cadelnita",
+            "<author> s-a cacat pe mormantu familiei ma-tii, <target>",
+            "<author> a chemat preotul sa predice lumii cat de prost este <target>"
+                    ],
+        "MORTI": [
+            "<target>, <author> ti-a futut toti mortii si ranitii... Cam necrofil... zic si eu..",
+            "<target>, <author> ti-a luat toti mortii-n pula"
+        ],
         "STATUT": [
             "<target>, <author> ti-a facut cadou o pula", 
-            "<author> si-a ancorat pula-n tine, <target>"
+            "<author> si-a ancorat pula-n tine, <target>",
+            "<author> a zis ca esti prost/proasta, <target>",
+            "<target> sa-l lingi in cur pe <author>",
+            "<author> ti-a pus pula-n brate, <target>"
                 ]
     }
 
